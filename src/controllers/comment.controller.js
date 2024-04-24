2100c7dcfa5af3d284b9c0e23dabf5d87adf9372
Sananthu-47 <sananthu47@gmail.com>
2024-04-25 02:38:10 +0530

    Get all comments & replies

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,95 +1,213 @@
-import {Comment} from "../models/comment.model.js";
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const addComment = asyncHandler(async(req,res,next)=>{
+const addComment = asyncHandler(async (req, res, next) => {
     try {
-        const {videoId} = req.params;
-        const {content, parentCommentId=null, mentions} = req.body;
-        if(!videoId) throw new ApiError(400, "Video id is missing");
-        if(!content) throw new ApiError(400, "Content is required");
-    
+        const { videoId } = req.params;
+        const { comment, parentCommentId = null, mentions } = req.body;
+        if (!videoId) throw new ApiError(400, "Video id is missing");
+        if (!comment) throw new ApiError(400, "Content is required");
+
         const owner = req.user?._id;
-    
-        if(!owner) throw new ApiError(400, "Please login to comment");
-    
-        const comment = await Comment.create({
+
+        if (!owner) throw new ApiError(400, "Please login to comment");
+
+        const comments = await Comment.create({
             videoId,
-            content,
+            comment,
             owner,
             parentCommentId,
-            mentions
+            mentions,
         });
-    
-        if(!comment) throw new ApiError(400, "Error while adding a comment");
-    
+
+        if (!comments) throw new ApiError(400, "Error while adding a comment");
+
         return res
-        .status(200)
-        .json(new ApiResponse(200, comment, "Comment added successfully"));
+            .status(200)
+            .json(new ApiResponse(200, comments, "Comment added successfully"));
     } catch (error) {
         console.log(error);
         next(error);
     }
-
 });
 
 const updateComment = asyncHandler(async (req, res, next) => {
     try {
-        const {content, mentions} = req.body;
-        const {commentId} = req.params;
+        const { comment, mentions } = req.body;
+        const { commentId } = req.params;
         const owner = req.user?._id;
 
-        if(!commentId) throw new ApiError(400, "Video id is missing");
-        if(!content) throw new ApiError(400, "Content cannot be empty");
-        if(!owner) throw new ApiError(400, "Please login to edit the comment");
+        if (!commentId) throw new ApiError(400, "Video id is missing");
+        if (!comment) throw new ApiError(400, "Content cannot be empty");
+        if (!owner) throw new ApiError(400, "Please login to edit the comment");
 
-        const comment = await Comment.findByIdAndUpdate(
+        const comments = await Comment.findByIdAndUpdate(
             commentId,
             {
                 $set: {
-                    content,
+                    comment,
                     mentions,
                 },
             },
             { new: true },
         );
 
-        if(!comment) throw new ApiError(400, "Error while updating the comment");
+        if (!comments)
+            throw new ApiError(400, "Error while updating the comment");
 
         return res
-        .status(200)
-        .json(new ApiResponse(200,comment,"Comment updated succesfully"));
-
+            .status(200)
+            .json(
+                new ApiResponse(200, comments, "Comment updated succesfully"),
+            );
     } catch (error) {
         console.log(error);
-        next(error)
+        next(error);
     }
-})
+});
 
 const deleteComment = asyncHandler(async (req, res, next) => {
     try {
-        const {commentId} = req.params;
+        const { commentId } = req.params;
         const owner = req.user?._id;
 
-        if(!commentId) throw new ApiError(400, "Video id is missing");
-        if(!owner) throw new ApiError(400, "Please login to edit the comment");
+        if (!commentId) throw new ApiError(400, "Video id is missing");
+        if (!owner) throw new ApiError(400, "Please login to edit the comment");
 
         const comment = await Comment.findByIdAndDelete(commentId);
 
-        if(!comment) throw new ApiError(400, "Error while deleting the comment");
+        if (!comment)
+            throw new ApiError(400, "Error while deleting the comment");
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, comment, "COmment deleted successfully"),
+            );
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+const getAllVideoComments = asyncHandler(async (req, res, next) => {
+    try {
+        const { videoId } = req.params;
+        const { page = 0, limit = 10 } = req.query;
+
+        if (!videoId) throw new ApiError(400, "Video id is missing");
+        const videoIdObject = new mongoose.Types.ObjectId(videoId);
+
+        const comments = await Comment.aggregate([
+            {
+                $match: {
+                    videoId: videoIdObject,
+                    parentCommentId: null,
+                },
+            },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "_id",
+                    foreignField: "parentCommentId",
+                    as: "replies",
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "user",
+                },
+            },
+            {
+                $project: {
+                    comment: 1,
+                    videoId: 1,
+                    repliesCount: { $size: "$replies" },
+                    username: { $arrayElemAt: ["$user.username", 0] },
+                    avatar: { $arrayElemAt: ["$user.avatar", 0] },
+                },
+            },
+            {
+                $skip: page * limit,
+            },
+            {
+                $limit: limit,
+            },
+        ]);
+
+        if (comments.length == 0) throw new ApiError(400, "No comments");
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, comments, "Comments fetched successfully"),
+            );
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+const getAllRepliesToComment = asyncHandler(async (req, res, next) => {
+    try {
+        const {videoId, parentCommentId} = req.params;
+        const { page = 0, limit = 10 } = req.query;
+        if(!videoId) throw new ApiError(400, "Video id is missing");
+        if(!parentCommentId) throw new ApiError(400, "ParentCommentId is missing");
+
+        const videoIdObject = new mongoose.Types.ObjectId(videoId);
+        const parentCommentIdObject = new mongoose.Types.ObjectId(parentCommentId);
+
+        const comments = await Comment.aggregate([
+            {
+                $match: {
+                    videoId: videoIdObject,
+                    parentCommentId: parentCommentIdObject
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "user",
+                }
+            },
+            {
+                $project: {
+                    comment: 1,
+                    username: {
+                      $arrayElemAt: ["$user.username", 0],
+                    },
+                    avatar: {
+                      $arrayElemAt: ["$user.avatar", 0],
+                    },
+                }
+            },
+            {
+                $skip: page * limit,
+            },
+            {
+                $limit: Number(limit),
+            },
+        ])
+
+        if(comments.length == 0) return res.status(200).json(new ApiResponse(200, "No replies to comment"));
 
         return res
         .status(200)
-        .json(new ApiResponse(200,comment,"COmment deleted successfully"));
+        .json(new ApiResponse(200, comments, "Replies fetched successfully"));
+
     } catch (error) {
         console.log(error);
         next(error)
     }
-})
+});
 
-export {
-    addComment,
-    updateComment,
-    deleteComment
-}
+export { addComment, updateComment, deleteComment, getAllVideoComments, getAllRepliesToComment };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, getAllRepliesToComment, getAllVideoComments, updateComment } from "../controllers/comment.controller.js";
 
 const router = Router();
 
@@ -9,5 +9,10 @@ router.route("/add-comment/:videoId").post(verifyJwt, addComment);
 router.route("/update-comment/:commentId").put(verifyJwt, updateComment);
 
 router.route("/delete-comment/:commentId").delete(verifyJwt, deleteComment);
+
+router.route("/get-all-comments/:videoId").get(getAllVideoComments);
+
+router.route("/get-all-replies/:videoId/:parentCommentId").get(getAllRepliesToComment);
+
 
 export default router;
